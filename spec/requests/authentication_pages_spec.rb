e8b2607b51d84ad1012adbe6ce25adf51b32d0af
Keita Ishibashi <fromjpn@gmail.com>
2014-01-02 21:48:33 +0900

    fix flash-message

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,10 +7,16 @@ describe "Authentication" do
   describe "signin" do
     before { visit signin_path }
 
+    #エラー原因をFlash表示
     describe "with invalid information" do
       before { click_button "Sign in" }
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe "with valid information" do
